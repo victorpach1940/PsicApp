0d6f6fbc26126255928a0ca830509ca0b4c19b40
Strider <victor.pachuca.1915@outlook.com>
2019-09-28 23:01:42 -0500

    visitas

---
+++ php/modal.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hiden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <form action="visitas.php" method="post">
+        <div class="modal-header">
+          Antes de empezar
+        </div>
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="edad">¿Cual es tu edad?</label>
+            <input type="text" class="form-control" name="edad" required="" pattern="(15|16|17|18|19|20|21|22|23|24)" maxlength="2" title="Edad real" placeholder="15-24">
+          </div>
+          <div class="form-group">
+            <label for="genero">¿Cual es tu genero?</label>
+            <input type="text" class="form-control" name="genero" required="" pattern="(F|M)" maxlength="1" title="F=femenino, M=masculino" placeholder="F o M">
+          </div>
+        </div>
+        <div class="modal-footer">
+            <button type="submit" class="btn btn-info" name="Listo">
+            Listo</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
---
+++ php/previus.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title>PsicApp-Inicio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=EB+Garamond&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+    <link rel="icon" href="../img/icono_page.png">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/8a020a1c5f.js" crossorigin="anonymous"></script>
+  </head>
+  <body id="preload">
+    <?php require 'modal.php'; ?>
+    <div class="container-fluid">
+      <div class="text-center" style="font-size: 60px;">
+        <img src="../img/brain.png" class="img-fluid" alt="PsicApp" style="padding-top: 20%;"><br>
+        Psic<span style="color:#333;"><em style="color:azure;">App</em></span><br>
+        <button type="button" class="btn btn-outline-dark" name="button" data-toggle="modal" data-target="#modal">Iniciar</button>
+      </div>
+    </div>
+  </body>
+</html>
