d8761c36dad0a0c4fe646275e8cc5a1684f90fd3
luismy <miguel.buny@gmail.com>
2019-09-29 03:31:53 -0500

    Se borraron los cuestionarios html, ahora son php, en los formularios de cada cuestionario son por el metodo porst con la siguiente descripcion -- Cuestionario adicciones con id cuestionario1, Cuestionario salud mental con id cuestionario2, cuestionario sexualidad con cuestionario3, y cuestionario violencia de pareja con id cuestionario4

---
+++ html/php copia/adicciones.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Adicciones</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/responsive-elements/1.0.2/responsive-elements.min.js" integrity="" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/responsive-nav.js/1.0.39/responsive-nav.min.js" integrity="" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/ResponsiveSlides.js/1.55/responsiveslides.min.js" integrity="" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="shrtcut icon" href="../img/ico.png">
+</head>
+<body>
+
+<nav>
+
+  <div class="nav-wrapper">
+     <a href="#" class="brand-logo">PsicApp</a>
+
+	<a href="#" class="sidenav-trigger" data-target="mobile-nav" >
+		<i class="material-icons">menu</i>
+	</a>
+     <ul class="right hide-on-med-and-down">
+
+       <li><a href="adicciones.php" class="red">Adicciones</a></li>
+       <li><a href="saludmental.php">Salud mental</a></li>
+       <li><a href="sexualidad.php">Sexualidad</a></li>
+       <li><a href="violenciadepareja.php">Violencia de pareja</a></li>
+       <li><a href="../index.php">Inicio</a></li>
+
+     </ul>
+  </div>
+</nav>
+
+
+   <ul class="sidenav" id="mobile-nav">
+   	  <li><a href="adicciones.php" class="red">Adicciones</a></li>
+       <li><a href="saludmental.php">Salud mental</a></li>
+       <li><a href="sexualidad.php">Sexualidad</a></li>
+       <li><a href="violenciadepareja.php">Violencia de pareja</a></li>
+       <li><a href="../index.html">Inicio</a></li>
+   </ul>
+
+  
+  <section>
+    
+  </section>
+
+  <div class="box container white
+  ">
+  <script>
+  	$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+</script>
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <article align="justify">
+  <h1>Adicciones</h1>
+			<h3>Introducción</h3>
+
+			<p >
+				El consumo de drogas cambia el funcionamiento del cerebro y provoca conductas fuera de lo normal. En PsicApp nuestros usuarios y pacientes son muy importantes a continuación le proporcionamos el siguiente indice
+						<div class="indice">
+			<p class="indice-Contenidos">Contenidos</p>
+			<ol class="indice-lista">
+				<li><a href="#1">¿Que es una adiccion?</a>
+				<ul>
+					<li><a href="#2">¿Cuál es la diferencia entre adicción física y adicción psicológica?</a> </li>
+					<li><a href="#3">Clasificación de las drogas</a> </li>
+				</li>
+				</ul>
+				<li><a href="#4">Medidas preventivas para las adicciones</a>
+					<ul>
+					<li><a href="#5">Las señales más frecuentes de que los menores pueden estar en contacto con las drogas</a> </li>
+					<li><a href="#6">Recomendaciones contra las Adicciones</a> </li>
+				
+					<li><a href="#7">¿Cómo se fortalece la autoestima de los menores?</a> </li>
+				</li>
+				</ul>
+				<li><a href="#8">Preguntas frecuentes</a></li>
+					</li>
+				</li>
+			</ol>
+					<!-- Esto es un comentario 
+
+					<ul>
+					<li><a href="#">¿Si una persona pide ayuda por dónde empieza?</a> </li>
+					<li><a href="#"></a>¿Que deberíamos buscar en un centro de tratamiento de adicciones</li>
+					<li><a href="#"></a>Me harán dejar de consumir drogas inmediatamente?</li>
+					<li><a href="#"></a>¿La drogadicción es tratada por un médico? </li>
+					<li><a href="#"></a>¿Qué tipo de consejería existe?</li>
+					<li><a href="#"></a>¿Existen medicamentos que pueden ayudar?</li>
+					<li><a href="#"></a>¿Qué ocurre cuando una persona ha estado en rehabilitación anteriormente?</li>
+					<li><a href="#"></a>¿Y si una persona está preocupada que otros lo descubran?</li>
+					<li><a href="#"></a>¿Qué sucede si una persona consume drogas porque se siente deprimida y detener su consumo empeore las cosas?</li>
+					<li><a href="#"></a>¿Cómo una persona puede hablar con otras con problemas similares?</li>
+					<li><a href="#"></a>Las personas dicen que no deberíamos consumir drogas y conducir, pero ¿qué pasa si se siente bien mientras conduce?</li>
+					<li><a href="#"></a>¿Dónde puedo encontrar información sobre drogas específicas?</li>
+				</li>
+				</ul>
+			-->
+			</ul>
+		</div>
+
+			
+
+			<h3 id="1">¿Que es una adiccion?</h3>
+				Se considera adicción, porque es difícil intentar dejar de consumirlas, ya que provocan alteraciones cerebrales en los mecanismos reguladores de la toma de decisiones y del control inhibitorio y porque el usuario de las mismas dedica gran parte de su tiempo en la búsqueda y consumo de ellas.
+		    <img class="responsive-img" src="../img/ad1.png" width="5000">
+			<h5 id="2">¿Cuál es la diferencia entre adicción física y adicción psicológica?</h5>
+			<ul>
+			  <li>Adicción física, ocurre en los sitios del cerebro donde las neuronas crean la necesidad del consumo compulsivo, debido a que el cuerpo se ha acostumbrado a la droga.</li>
+			  <li>Adicción psicológica, es la necesidad de consumo de una sustancia, que se manifiesta a nivel de pensamientos o emociones, ante una situación estresante, o algún problema. Por lo tanto no existe dependencia física, debido a que no se desarrollan receptores a nivel neuronal para la acción de la sustancia adictiva.</li>
+
+			  
+			</ul>
+		<h5 id="3">Clasificación de las drogas</h5>
+		<ul>
+			<li>Eufóricas</li>
+			Opio y sus derivados, así como cocaína
+			<li>Fantásticas</li>
+			Mezcalina, marihuana y beleño, entre otras
+			<li>Embriagantes</li>
+			Alcohol, éter, cloroformo y bencina
+			<li>Hipnóticas</li>
+			Barbitúricos y otros somníferos
+			<li>Excitantes</li>
+			Cafeína y tabaco, entre otras
+			</ul>
+			A la lista se suman las “nuevas drogas o drogas de diseño” creadas con fines recreativos, entre las que se encuentran: feniletilaminas, arilhexilaminas, opiáceos, derivados del fentanilo, la meperidina y la metacualona.
+
+		<h3 id="4">Medidas preventivas para las adicciones</h3>
+		Las adicciones pueden afectar a hombres y mujeres de cualquier edad, nivel de educación o clase social. Una adicción es cuando la persona siente el deseo incontrolable de recurrir al consumo de determinadas sustancias, por lo general químicas, ya sea de modo continúo o periódico. Es decir, la persona adicta a una sustancia no puede controlar el deseo de utilizarla. La adicción al alcohol y las drogas debe ser vista como un problema que afecta no sólo al que se intoxica con esas sustancias, sino como un conflicto de todo el grupo familiar al que pertenece el adicto.<br /><em>El cariño y la atención hacia los menores juegan un papel muy importante en la prevención de las adicciones. Si los niños y niñas crecen con amor y seguridad, si tienen confianza para comunicarse, si se sienten comprendidos y valorados, pero además en la familia no hay adicciones, será difícil que busquen el camino de las drogas.</em><br />
+ 				    <img class="responsive-img" src="../img/ad2.jpg" width="5000">
+
+
+ 		
+ 			<h5><strong id="5">Las señales más frecuentes de que los menores pueden estar en contacto con las drogas son:</strong></h5>
+ 			<ul>
+ 				<li>Tendencia a la soledad.</li>
+ 				<li>Cambios constantes de conducta.</li>
+ 				<li>Dificultad para asumir responsabilidades.</li>
+ 				<li>Disminución del rendimiento escolar.</li>
+ 				<li>Cambio repentino de amistades.</li>
+ 				<li>Falta de interés en actividades que antes le gustaban.</li>
+ 				<li>Duerme mucho o casi no duerme.</li>
+ 				<li>Siempre necesita dinero o tiene mucho dinero.</li>
+ 				<li>Disminuye su apetito.</li>
+ 				<li>Dice mentiras.</li>
+ 				<li>Hay pequeños hurtos en casa.</li>
+ 			</ul>
+ 					    <img class="responsive-img" src="../img/ad3.jpg" width="5000">
+
+ 		<h5 id="6">Recomendaciones contra las Adicciones</h5>
+			Para prevenir el fenómeno adictivo se recomienda:
+			<ol>
+				<li>Integrar a la Familia en un ambiente de comunicación, respeto y confianza.</li>
+				<li>Establecer lazos de afecto y convivencia positiva con familiares, grupos de amigos,vecinos, maestros etc..</li>
+				<li>Reconocer a nuestros hijos los logros, habilidades y capacidades personales.</li>
+				<li>Crear el hábito del estudio, lectura y participación en actividades fuera de la escuela.</li>
+			</ol>
+			<h5><b id="7">Recuerde que la autoestima de los menores se fortalece:</b></h5>
+			<ol>
+				<li>Demostrando interés por sus gustos.</li>
+				<li>Aceptándolos, sin compararlos con nadie.</li>
+				<li>Ayudándolos a obtener confianza en sí mismos, autovalorarse y quererse.</li>
+				<li>Aprovechando el tiempo libre para la convivencia con ellos.</li>
+				<li>Expresándoles cariño y respeto</li>
+			</ol>
+			<img class="responsive-img" src="../img/ad4.jpg" width="5000">
+			<a href="#top">Regresar al inicio</a>
+		<h3 id="8">Preguntas frecuentes</h3>
+			<b>1. ¿Si una persona pide ayuda por dónde empieza?</b><br>
+			El primer paso importante es pedir ayuda. La visita a un médico para una posible referencia al tratamiento es una forma de hacerlo. El médico puede ser llamado con antelación y le puede preguntar si él o ella se siente cómodo(a) al discutir de la prueba de detección y tratamiento del  abuso de drogas. De no ser así, solicítele una referencia con otro médico. Usted también puede contactar a un especialista en drogadicción. Se necesita mucho valor para buscar ayuda por un problema de drogadicción u otra adicción porque se espera un trabajo duro por delante. Sin embargo, el tratamiento puede funcionar, y las personas se recuperan a diario de sus adicciones.<br>
+			<b>2. ¿Que deberíamos buscar en un centro de tratamiento de adicciones</b><br>
+			Los enfoques de tratamiento deben ser diseñados para dirigir cada patrón de abuso de las drogas del paciente y también sus problemas médicos, psiquiátricos y sociales relacionados con la droga. Algunos centros de tratamiento ofrecen programas de tratamiento ambulatorio, lo que permite que usted continúe desempeñando algunas de sus responsabilidades diarias. Sin embargo, algunas personas les van mejor con tratamiento hospitalario (residencial). Un especialista en drogadicción puede asesorar a usted o su ser querido sobre las mejores opciones.</br>
+			<b>3. Me harán dejar de consumir drogas inmediatamente?</b><br>
+			El primer paso en el tratamiento es la desintoxicación, lo que ayuda a la persona a eliminar todas las drogas de su sistema. Esto es importante porque las drogas afectan las capacidades mentales que usted necesita para continuar en tratamiento. Cuando los pacientes suspenden el consumo de drogas, ellos pueden experimentar una variedad de síntomas del síndrome de abstinencia, incluso depresión, ansiedad y otros trastornos del estado de ánimo; inquietud; e insomnio. Los centros de tratamiento tienen mucha experiencia en ayudar a una persona sobrellevar este proceso, y mantenerlo a salvo. Dependiendo de la droga a la que usted esté adicto, también puede haber medicamentos que le harán sentir un poco mejor durante la abstinencia de drogas, lo que hace más fácil de dejar de consumirlas.<br>
+			<b>4. ¿La drogadicción es tratada por un médico?</b></br>
+			Existen diferentes tipos de especialistas en drogadicción involucrados en la atención a las adicciones, incluso médicos, enfermera(o)s, terapeutas y trabajadores sociales, entre otros. En ciertos programas de tratamiento, varios especialistas trabajan en equipo para ayudar a que le recupere de su adicción.</br>
+			<b>5. ¿Qué tipo de consejería existe?</b></br>
+			El tratamiento conductual, también conocido como “psicoterapia,” ayuda a los pacientes a comprometerse en el proceso de tratamiento, cambiar sus actitudes y comportamientos relacionados al abuso de las drogas, y ampliar las posibilidades de llevar una vida saludable. Estos tratamientos también pueden aumentar la eficacia de los medicamentos y ayudar a las personas a seguir en tratamiento por más tiempo. El tratamiento para el abuso de drogas y drogadicción puede darse usando diferentes enfoques de comportamiento. En PsicApp puede contactar con uno de nuestros expertos sobre los diferentes tipos de terapia y otros tratamientos conductuales.</br>
+			<b>6. ¿Existen medicamentos que pueden ayudar?</b></br>
+			Hay medicamentos disponibles para tratar las adicciones al alcohol, la nicotina y los opioides (heroína y analgésicos). Otros medicamentos están disponibles para tratar posibles enfermedades mentales, como la depresión, que pueden estar contribuyendo a la adicción. Además, el medicamento que no es adictivo es prescrito a veces para ayudar con la abstinencia. Cuando el medicamento está disponible, puede ser combinado con la terapia conductual para asegurar el éxito de la mayoría de los pacientes. El proveedor del tratamiento le aconsejará a usted y a su ser querido cuales son los medicamentos apropiados y que están disponibles. Algunos centros de tratamiento siguen la filosofía de que ellos no deberían tratar la drogadicción con otras drogas, pero investigaciones demuestran que los medicamentos pueden ayudar en muchos casos</br>
+			<b>7. ¿Qué ocurre cuando una persona ha estado en rehabilitación anteriormente?</b></br>
+			Esto significa que la persona ya ha aprendido muchas de las destrezas necesarias para recuperarse de la adicción y debería intentarlo de nuevo. La recaída no debería desanimar a nadie que decida realizar otro intento. Las tasas de recaída por adicción son similares a las tasas por otras enfermedades crónicas que muchas personas padecen, como hipertensión, diabetes y asma. El tratamiento de las enfermedades crónicas implica cambiar los comportamientos profundamente arraigados, y a veces, la recaída viene incluida, pero no significa que el tratamiento fracasó. Un regreso al abuso de las drogas indica que la persona necesita comenzar el tratamiento de nuevo, o modificarlo o que pueda beneficiarse desde una perspectiva diferente.</br>
+			<b>8. ¿Y si una persona está preocupada que otros lo descubran?</b></br>
+			Una persona que esté buscando tratamiento para la adicción puede decirle a su patrón o sus amigos que necesita irse por razones médicas. Si usted habla con el médico u otro experto en medicina, las leyes de privacidad les prohíbe divulgar su información médica con cualquier persona fuera del sistema de salud sin su autorización. Además, la mayoría de los médicos especializados en tratamiento para la adicción no pueden compartir su información con nadie, hasta otros médicos, sin 
+			su permiso por escrito.</br>
+			<b>9. ¿Qué sucede si una persona consume drogas porque se siente deprimida y detener su consumo empeore las cosas?</b></br>
+			Es muy probable que usted necesite buscar tratamiento tanto para la depresión como para la adicción. Esto sucede a menudo. Se le llama “comorbilidad,” o “morbilidad asociada” cuando usted tiene más de un problema de salud a la misma vez. Es importante que discuta todo sus síntomas y comportamientos con su médico. Hay muchas drogas que no son adictivas y que pueden ayudar contra la depresión u otras enfermedades mentales. A veces, los proveedores de cuidado de salud no se comunican entre ellos como deberían, lo que significa que usted puede ser su mejor defensor y asegurar de que todos sus proveedores de asistencia médica sepan los problemas de salud que le preocupa. Las personas que tienen problemas concurrentes deben ser tratadas por todos ellos al mismo tiempo. Nota: si usted se siente tan deprimido que ha considerado hacerse daño, puede visitar el siguiente enlace de ayuda: <a href="saludmental.html" target="_blank">Salud mental</a></li> o bien puede ponerse en contacto con uno de nuestros expertos con ayuda del chat. </br>
+			<b>10. ¿Cómo una persona puede hablar con otras con problemas similares?</b></br>
+			Los grupos de autoayuda pueden prolongar los efectos de tratamiento profesional. Los grupos de autoayuda más conocidos son aquellos afiliados con Alcohólicos Anónimos (AA), Narcóticos Anónimos (NA) y Cocaína Anónima (CA), todos los cuales son basados en un modelo de 12 pasos. La mayoría de los programas de tratamientos de adicción a las drogas estimulan a los pacientes a que participen en un grupo de autoayuda durante y después de un tratamiento oficial. Estos grupos en particular pueden ser muy útiles durante la recuperación ya que sirven como una constante fuente de apoyo para mantenerse libre de drogas. </br>
+			<b>11. Las personas dicen que no deberíamos consumir drogas y conducir, pero ¿qué pasa si se siente bien mientras conduce?</b></br>
+			Lo más responsable que una persona puede hacer es dejar de conducir mientras consume drogas. Esto puede ser un inconveniente para usted, pero podría salvar su vida y la de otros. Ciertas drogas actúan diferente en el cerebro, pero todas perjudican las habilidades necesarias para el funcionamiento seguro del vehículo. Estas incluyen las habilidades motoras, el equilibrio y la coordinación, la percepción, la atención, el tiempo de reacción y el juicio. Pequeñas cantidades de algunas drogas pueden incluso tener efectos que alteren la capacidad de manejar un vehículo. Las drogas también afectan su capacidad de diagnosticar si tiene problemas por lo que no debe confiar en su propio juicio sobre manejar un vehículo hasta que reciba una evaluación y tratamiento</br>
+			<b>12. ¿Dónde puedo encontrar información sobre drogas específicas?</b></br>
+			El NIDA también tiene una página web de fácil lectura con<a href="https://www.drugabuse.gov/es/informacion-sobre-drogas" target="_blank"> información sobre una gran lista de drogas, y las más comunes están disponibles en español.</a></br>
+			<b> En caso de tener dudas al respecto o no sabe que hacer si sus hijos o usted ya están ligados a las adicciones, puede solicitar información en el chat en linea de PsicApp, donde uno de nuestros expertos lo atendera.</b>
+		</p>
+	</div>
+		</article>
+	</section>
+  </div>
+</body>
+</html>
+<!-- script -->
+
+
+  <script>
+     
+     $(document).ready(function(){
+          
+          $(window).scroll(function(){
+
+            if($(window).scrollTop()>300){
+              $('nav').addClass('red');
+            }else{
+              $('nav').removeClass('red');
+            }
+
+          });
+
+     });
+
+  </script>
+  <script>
+  		     $(document).ready(function(){
+  		     	$('.sidenav').sidenav();
+  		     });
+
+
+  </script>
+ 
+
+
+<!-- style -->
+<style>
+    
+    nav{
+      position: fixed;
+      background: rgba(0, 0, 0, 0.2);
+      padding:0px 20px;
+    }
+
+    section{
+      background-image: url(back.jpg);
+      background-size: cover;
+      width: 100%;
+      height: 800px;
+    }
+    .box{
+      margin-top: 20px;
+      height: 1000px;
+    }
+
+    nav li a:hover{
+      background: red;
+    }
+</style>
